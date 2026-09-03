cb66988e9eee6b61986a3b1d1d919d719e9beaf3
Ryanpenaa <48598330+Ryanpenaa@users.noreply.github.com>
2026-09-02 20:40:23 -0400

    Add Meta Pixel page view tracking

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -112,6 +112,27 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
   errorComponent: ErrorComponent,
 });
 
+function MetaPixel() {
+  return (
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version="2.0";n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,"script","https://connect.facebook.net/en_US/fbevents.js");fbq("init","925310143958238");fbq("track","PageView");`,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=925310143958238&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
+    </>
+  );
+}
+
 function RootShell({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
@@ -119,6 +140,7 @@ function RootShell({ children }: { children: ReactNode }) {
         <HeadContent />
       </head>
       <body>
+        <MetaPixel />
         {children}
         <Scripts />
       </body>
